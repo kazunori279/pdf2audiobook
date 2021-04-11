568ef108755a70bcdaab84f9b434087e24de9204
stef <stef@kotchan.fun>
2021-04-11 15:59:12 +0000

    deploy_all.sh

---
+++ deploy_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gcloud services enable cloudfunctions.googleapis.com
+gcloud services enable vision.googleapis.com          
+gcloud services enable cloudapis.googleapis.com     
+gcloud services enable automl.googleapis.com 
